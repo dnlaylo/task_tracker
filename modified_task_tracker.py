878125334c7135e12d0e995547708d32bc2389cf
dnlaylo <laylomarga@gmail.com>
2025-01-25 01:06:27 +0800

    made a canvas and ran the program

--- modified_task_tracker.py
+++ modified_task_tracker.py
@@ -2,3 +2,16 @@ import tkinter as tk
 from tkinter import ttk, messagebox
 from ttkbootstrap import Style
 import json
+
+class TaskTracker(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("Meliora Task Tracker")
+        self.geometry("400x400")
+        style = Style(theme="darkly")
+        style.configure("Custon.TEntry", foreground="gray")
+
+if __name__ == '__main__':
+    app = TaskTracker()
+    app.mainloop()
