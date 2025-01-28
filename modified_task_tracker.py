14ade7a894673bd6999c6f8fea42f6cac4d002ab
dnlaylo <laylomarga@gmail.com>
2025-01-28 14:40:20 +0800

    brought back other functions

--- modified_task_tracker.py
+++ modified_task_tracker.py
@@ -66,15 +66,15 @@ class TaskTracker(tk.Tk):
             self.task_lists[category] = task_list # call array then assign to listbox
 
         # Edit Button
-        #ttk.Button(self, text="Edit", style="info.TButton", command=self.edit_task).pack(side=tk.LEFT, padx=10, pady=10)
+        ttk.Button(self, text="Edit", style="info.TButton", command=self.edit_task).pack(side=tk.LEFT, padx=10, pady=10)
 
         # buttons for done, delete, and progress
-        #ttk.Button(self, text="Mark done", style="success.TButton",
-        #          command=self.mark_done).pack(side=tk.LEFT, padx=10, pady=10)
-        #ttk.Button(self, text="Delete", style="danger.TButton",
-        #          command=self.delete_task).pack(side=tk.LEFT, padx=10, pady=10)
-        #ttk.Button(self, text="View Progress", style="info.TButton",
-        #          command=self.progress).pack(side=tk.RIGHT, padx=10, pady=10)
+        ttk.Button(self, text="Mark done", style="success.TButton",
+                  command=self.mark_done).pack(side=tk.LEFT, padx=10, pady=10)
+        ttk.Button(self, text="Delete", style="danger.TButton",
+                  command=self.delete_task).pack(side=tk.LEFT, padx=10, pady=10)
+        ttk.Button(self, text="View Progress", style="info.TButton",
+                  command=self.progress).pack(side=tk.RIGHT, padx=10, pady=10)
         
         self.load_tasks()
 
@@ -125,82 +125,82 @@ class TaskTracker(tk.Tk):
         with open("tasks.json", "w") as f:
             json.dump(data, f)
 
-    #def edit_task(self):
-    #    task_index = self.task_list.curselection() # get what is clicked
-    #    if task_index: 
-    #        chosen_task = self.task_list.get(task_index)
-    #        separate_data = chosen_task.split(" | ") # separate task name, category, priority
-    #        
-    #        # create a new window for edit
-    #        edit_window = tk.Toplevel(self)
-    #        edit_window.title("Edit Task")
-    #
-    #        # input box
-    #        self.edit_name = ttk.Entry(edit_window, font=(
-    #            "Futura", 12), width=30, style="Custon.TEntry")
-    #        self.edit_name.insert(0, separate_data[0]) # input current chosen task
-    #        self.edit_name.pack(padx=10, pady=10)
-    #
-    #        # category
-    #        self.edit_category = ttk.Combobox(edit_window, values = ["Work", "Personal", "School"])
-    #        self.edit_category.set(separate_data[1])
-    #        self.edit_category.pack(pady=5)
-    #
-    #        # priority
-    #        self.edit_priority = ttk.Combobox(edit_window, values = ["Very Important!", "Not so Important"])
-    #        self.edit_priority.set(separate_data[2])
-    #        self.edit_priority.pack(pady=5)
-    #
-    #        # deadline
-    #        self.edit_deadline = ttk.Entry(edit_window, font = "Futura")
-    #        self.edit_deadline.insert(0, separate_data[3])
-    #        self.edit_deadline.pack(pady=5)
-    #
-    #        # save button
-    #        ttk.Button(edit_window, text = "Save", command = lambda: self.save_changes(task_index, edit_window)).pack(pady=5) #lambda - inner function so that i can use the variables for an outside function
+    def edit_task(self):
+        task_index = self.task_list.curselection() # get what is clicked
+        if task_index: 
+            chosen_task = self.task_list.get(task_index)
+            separate_data = chosen_task.split(" | ") # separate task name, category, priority
+            
+            # create a new window for edit
+            edit_window = tk.Toplevel(self)
+            edit_window.title("Edit Task")
+    
+            # input box
+            self.edit_name = ttk.Entry(edit_window, font=(
+                "Futura", 12), width=30, style="Custon.TEntry")
+            self.edit_name.insert(0, separate_data[0]) # input current chosen task
+            self.edit_name.pack(padx=10, pady=10)
+    
+            # category
+            self.edit_category = ttk.Combobox(edit_window, values = ["Work", "Personal", "School"])
+            self.edit_category.set(separate_data[1])
+            self.edit_category.pack(pady=5)
+    
+            # priority
+            self.edit_priority = ttk.Combobox(edit_window, values = ["Very Important!", "Not so Important"])
+            self.edit_priority.set(separate_data[2])
+            self.edit_priority.pack(pady=5)
+    
+            # deadline
+            self.edit_deadline = ttk.Entry(edit_window, font = "Futura")
+            self.edit_deadline.insert(0, separate_data[3])
+            self.edit_deadline.pack(pady=5)
+    
+            # save button
+            ttk.Button(edit_window, text = "Save", command = lambda: self.save_changes(task_index, edit_window)).pack(pady=5) #lambda - inner function so that i can use the variables for an outside function
 
-    #def save_changes(self, task_index, edit_window):
-    #    new_name = self.edit_name.get()
-    #    new_category = self.edit_category.get()
-    #    new_priority = self.edit_priority.get()
-    #    new_deadline = self.edit_deadline.get()
-    #
-    #    new_data = f"{new_name} | {new_category} | {new_priority} | {new_deadline}"
-    #    self.task_list.delete(task_index)
-    #    self.task_list.insert(task_index, new_data)
-    #
-    #    if new_priority == "Very Important!":
-    #        self.task_list.itemconfig(task_index, fg="red")
-    #    else:
-    #        self.task_list.itemconfig(task_index, fg="green")
-    #
-    #    self.save_tasks() # called save_tasks function
-    #    edit_window.destroy() # destroy window once saved
-    #        
-    #def progress(self):
-    #    done_count = 0
-    #    total_count = self.task_list.size()
-    #    for i in range(total_count):
-    #        if self.task_list.itemcget(i, "fg") == "gray":
-    #            done_count += 1
-    #    messagebox.showinfo("Task Progress", f"Total tasks: {total_count}\nCompleted tasks: {done_count}")
+    def save_changes(self, task_index, edit_window):
+        new_name = self.edit_name.get()
+        new_category = self.edit_category.get()
+        new_priority = self.edit_priority.get()
+        new_deadline = self.edit_deadline.get()
+    
+        new_data = f"{new_name} | {new_category} | {new_priority} | {new_deadline}"
+        self.task_list.delete(task_index)
+        self.task_list.insert(task_index, new_data)
+    
+        if new_priority == "Very Important!":
+            self.task_list.itemconfig(task_index, fg="red")
+        else:
+            self.task_list.itemconfig(task_index, fg="green")
+    
+        self.save_tasks() # called save_tasks function
+        edit_window.destroy() # destroy window once saved
+            
+    def progress(self):
+        done_count = 0
+        total_count = self.task_list.size()
+        for i in range(total_count):
+            if self.task_list.itemcget(i, "fg") == "gray":
+                done_count += 1
+        messagebox.showinfo("Task Progress", f"Total tasks: {total_count}\nCompleted tasks: {done_count}")
 
-    #def mark_done(self):
-    #    task_index = self.task_list.curselection()
-    #    current = self.task_list.get(task_index)
+    def mark_done(self):
+        task_index = self.task_list.curselection()
+        current = self.task_list.get(task_index)
 
-    #    if task_index:
-    #        task_done = f"{current} | DONE"
-    #        self.task_list.delete(task_index)
-    #        self.task_list.insert(task_index, task_done)
-    #        self.task_list.itemconfig(task_index, fg="gray")
-    #        self.save_tasks()
-    #
-    #def delete_task(self):
-    #    task_index = self.task_list.curselection()
-    #    if task_index:
-    #        self.task_list.delete(task_index)
-    #        self.save_tasks()
+        if task_index:
+            task_done = f"{current} | DONE"
+            self.task_list.delete(task_index)
+            self.task_list.insert(task_index, task_done)
+            self.task_list.itemconfig(task_index, fg="gray")
+            self.save_tasks()
+    
+    def delete_task(self):
+        task_index = self.task_list.curselection()
+        if task_index:
+            self.task_list.delete(task_index)
+            self.save_tasks()
 
     def load_tasks(self):
         try:
